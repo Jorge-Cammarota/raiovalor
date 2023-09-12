148ffb44c823dec4af0649698ec3fb434da7d44a
Jorge-Cammarota <Jorge-Cammarota>
2023-09-12 11:51:53 -0300

    circuloraio

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um Valor");
+
+        double R, A, pi = 3.14159;
+
+        R = sc.nextDouble();
+
+        A = pi * R * R;
+
+        System.out.printf("A=%.4f%n", A);
+
+        sc.close();
+    }
+}
+
